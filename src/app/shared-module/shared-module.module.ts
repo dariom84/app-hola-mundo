c1ac520b2a642d531e295587f0c1f183496ae236
Dariom84 <dariom84@gmail.com>
2019-08-20 19:01:30 -0300

    s

--- src/app/shared-module/shared-module.module.ts
+++ src/app/shared-module/shared-module.module.ts
@@ -1,17 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TaskComponent } from './components/task/task.component';
+import { HeaderComponent } from './components/header/header.component';
 import { AddTaskComponent } from './components/add-task/add-task.component';
 import { TasksListComponent } from './components/tasks-list/tasks-list.component';
-import { HeaderComponent } from './components/Header/header.component';
+import { AlertModule } from 'ngx-bootstrap/alert';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 @NgModule({
-  declarations: [TaskComponent, AddTaskComponent, TasksListComponent, HeaderComponent],
-  imports: [
-    CommonModule
-  ],
-  exports:[
-    TaskComponent, AddTaskComponent, TasksListComponent, HeaderComponent
-  ]
+ declarations: [TaskComponent, AddTaskComponent, TasksListComponent,HeaderComponent],
+ imports: [
+   CommonModule,
+   ReactiveFormsModule,
+   AlertModule.forRoot(),
+   ModalModule.forRoot(),
+   BsDatepickerModule.forRoot()
+ ],
+ exports:[
+   TaskComponent, AddTaskComponent, TasksListComponent,HeaderComponent
+ ]
 })
+
 export class SharedModule { }
