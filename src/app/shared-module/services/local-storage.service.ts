2dc7e066e23c258543ecb320905256fd63ab33be
Dariom84 <dariom84@gmail.com>
2019-08-27 19:43:05 -0300

    d

--- src/app/shared-module/services/local-storage.service.ts
+++ src/app/shared-module/services/local-storage.service.ts
@@ -1,30 +1,24 @@
 import { Injectable } from '@angular/core';
-
 @Injectable({
-  providedIn: 'root'
+ providedIn: 'root'
 })
 export class LocalStorageService {
-
-  private store(key: any, obj: any): void {
-    localStorage.setItem(key, JSON.stringify(obj));
-  }
-
-  public retrieve(key: any) {
-    let itemString = localStorage.getItem(key);
-    return JSON.parse(itemString);
-  }
-
-  public create(key: any, obj: any = {}): string {
-    this.store(key, obj)
-    return key;
-  }
-
-  public update(key: any, obj: any): void {
-    let item = this.retrieve(key);
-    this.store(key, Object.assign(item, obj));
-  }
-
-  public delete(key: any): void {
-    localStorage.removeItem(key);
-  }
+ private store(key: any, obj: any): void {
+   localStorage.setItem(key, JSON.stringify(obj));
+ }
+ public retrieve(key: any) {
+   let itemString = localStorage.getItem(key);
+   return JSON.parse(itemString);
+ }
+ public create(key: any, obj: any = {}): string {
+   this.store(key, obj)
+   return key;
+ }
+ public update(key: any, obj: any): void {
+   let item = this.retrieve(key);
+   this.store(key, Object.assign(item, obj));
+ }
+ public delete(key: any): void {
+   localStorage.removeItem(key);
+ }
 }
